bfb682fd78dbcbbd25161fcc33e1449530aaff0e
joao <joaopaulo94.economia@gmail.com>
2024-04-05 07:25:00 -0300

    Função para gerar um slug

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,42 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/slug-generate";
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello world'
+const prisma = new PrismaClient({
+    log: ['query'],
 })
 
-app.get('/teste', () => {
-    return 'Hello world'
+app.post('/events', async (req, res) => {
+    // validação dos dados
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+    // comparacao - recebido(req.body) x esperado(createEventSchema)
+    const data = createEventSchema.parse(req.body)
+
+    //slug
+    const slug = generateSlug(data.title)
+
+    // inserindo os dados na tabela
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: slug,
+        }
+    }) 
+
+    return res.status(201).send({ eventId:  event.id })
+    
 })
+
+
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
